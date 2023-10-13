979df263c35512d93341e709d8eb36f15881b214
Tiago Cunha <91148044+Tiago-msC@users.noreply.github.com>
2023-10-13 18:08:20 -0300

    Revert "St00002"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,2 @@
-<script lang="ts">
-  import '../styles.css';
-
-</script>
-
 <h1>Bem vindo ao pescouJobs</h1>
 <p>Para começar, crie uma conta ou faça login</p>
-
-<style>
-  h1 {
-    font-family: var(--theme-typography-font-family-semibold);
-    /* font-weight: 800; */
-  }
-  
-</style>
