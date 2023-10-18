ac98e075eb41e02e940ca7d44f57af2433e14bc1
vitor Gabriel <vitor_gabriel.4815.7@gmail.com>
2023-10-18 18:56:28 -0300

    ajustando estilo da lista

--- src/routes/components/ServiceCategories.svelte
+++ src/routes/components/ServiceCategories.svelte
@@ -1,31 +1,39 @@
 <title>ServiceCategories</title>
 
+
 <style>
     body{
         border: 0;
         margin-inline-start: 0;
         margin-inline-end: 0;
     }
+    
     ul{
         display: flex;
         flex-wrap: wrap;
-        border-top: 1px solid #000;
-        border-bottom: 1px solid #000;
-        justify-content: space-between;
+        border-top: 1px solid #5F5F5F;
+        border-bottom: 1px solid #5F5F5F;
+        justify-content: space-between;    
     }
 
     li{
         display: inline;
-        margin-right: 8px;
+        margin-right: 30px;
         margin-top: 10px;
-        margin-bottom: 10px;
+        margin-bottom: 10px;      
     }
 
     a{
         padding: 5px;
-        color: black;
+        color: rgb(0, 0, 0);
         font-weight: bolder;
         text-decoration: none;
+
+        font-family: 'Poppins';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 18px;
+        line-height: 27px;
     }
 </style>
 
