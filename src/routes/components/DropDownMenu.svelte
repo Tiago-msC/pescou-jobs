6fd32cdabf777b6acf00bd57afb343b692d7f4f6
vitor Gabriel <vitor_gabriel.4815.7@gmail.com>
2023-10-19 13:16:02 -0300

    iniciando DropDownMenu

---
+++ src/routes/components/DropDownMenu.svelte
@@ -0,0 +1,25 @@
+<title>Drop Down Menu</title>
+
+<body>
+    <div class="dropdown">        
+        <select class="menu">  
+            <option disabled selected>Para prestadores</option>
+            <option>Área do prestador</option> 
+            <option>Torne-se um prestador</option> 
+        </select>
+    </div>
+</body>
+
+<style>
+    .menu{
+        font-family: 'Poppins';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 24px;
+
+        border: 0;
+        outline: 0;
+        }
+
+</style>
