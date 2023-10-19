ce8381c6cf4defef1e41a4ae4acd5be6f6019544
Samuel-Duque <samuellocatel@gmail.com>
2023-10-19 18:33:30 -0300

    Inclusao do componente Header

---
+++ src/components/Header.svelte
@@ -0,0 +1,46 @@
+<script>
+    import logo from "$lib/images/pescoujobs-logo.svg"
+</script>
+
+<header>
+    <img src={logo} alt="Logo PescouJobs" class="pescoujobs-logo"/>
+    <nav>
+        <div class="container-nav">
+            <ul>
+                <li>Para prestadores</li>
+                <li>Ajuda</li>
+                <li>Cadastrar</li>
+            </ul>
+            <button>Entrar</button>
+        </div>
+    </nav>
+</header>
+<style>
+    header{
+        display: flex;
+        justify-content: space-between;
+        height: 96px;
+        align-items: center;
+        padding: 0 48px;
+        border-bottom: 1px solid var(--theme-border-color-primary);
+        
+    }
+    ul{
+        display: flex;
+        margin-right: 24px;
+    }
+    li{
+        list-style: none;   
+        margin-left: 34px;
+
+    }
+    .container-nav{
+        display: flex;
+    }
+    .pescoujobs-logo{
+        height: 45px;
+        width: 202px;
+        object-fit: contain;
+
+    }
+</style>
