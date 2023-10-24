a9159907cb17db89d9b7891550223cc368da2109
Tiago Cunha <tiagomscunha@outlook.com>
2023-10-24 19:23:34 -0300

    Melhorias no header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,49 +1,106 @@
 <script>
-    import logo from "$lib/images/pescoujobs-logo.svg"
+  import logo from "$lib/assets/icons/pescoujobs-logo.svg";
+  import logoRetractable from "$lib/assets/icons/pescoujobs-logo-retractable.svg";
   import Button from "./Button.svelte";
-  import account from "$lib/images/icons/account.svg"
+  import account from "$lib/assets/icons/account.svg";
 </script>
 
 <header>
-    <img src={logo} alt="Logo PescouJobs" class="pescoujobs-logo"/>
-    <nav>
-        <div class="container-nav">
-            <ul>
-                <li>Para prestadores</li>
-                <li>Ajuda</li>
-                <li>Cadastrar</li>
-            </ul>
-            <Button text="Entrar" type="outline" iconLeft={account}></Button>
-        </div>
-    </nav>
+  <!-- <img src={logoRetractable} alt="Logo PescouJobs" class="pescoujobs-logo-retractable"/> -->
+  <nav id="desktop-menu">
+    <img src={logo} alt="Logo PescouJobs" class="pescoujobs-logo" />
+    <div class="container-nav">
+      <ul>
+        <li><a href="/">Para prestadores</a></li>
+        <li><a href="/">Ajuda</a></li>
+        <li><a href="/">Cadastrar</a></li>
+      </ul>
+      <Button
+        text="Entrar"
+        notClickAnimation
+        type="outline"
+        iconLeft={account}
+      />
+    </div>
+  </nav>
+  <nav id="mobile-menu">
+    <img src={logoRetractable} alt="Logo PescouJobs" class="pescoujobs-logo-retractable" />
+  </nav>
 </header>
+
 <style>
-    header{
-        display: flex;
-        justify-content: space-between;
-        height: 96px;
-        align-items: center;
-        padding: 0 48px;
-        border-bottom: 1px solid var(--theme-border-color-primary);
-        
-    }
-    ul{
-        display: flex;
-        margin-right: 24px;
-    }
-    li{
-        list-style: none;   
-        margin-left: 34px;
+  header {
+    border-bottom: 1px solid var(--theme-border-color-primary);
+  }
 
-    }
-    .container-nav{
-        display: flex;
-        align-items: center;
-    }
-    .pescoujobs-logo{
-        height: 45px;
-        width: 202px;
-        object-fit: contain;
+  nav {
+    height: 96px;
+    padding: 0 48px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 
+  #mobile-menu {
+    display: none;
+  }
+
+  ul {
+    display: flex;
+    margin-right: 24px;
+  }
+  li {
+    list-style: none;
+    margin-left: 34px;
+  }
+  .container-nav {
+    display: flex;
+    align-items: center;
+  }
+  .pescoujobs-logo,
+  .pescoujobs-logo-retractable {
+    height: 40px;
+    object-fit: contain;
+  }
+
+  .pescoujobs-logo-retractable {
+    display: none;
+  }
+  a {
+    text-decoration: none;
+    color: var(--theme-text-color-primary);
+    font-size: 0.85rem;
+    font-weight: 500;
+  }
+
+  @media (max-width: 768px) {
+    nav {
+      padding: 0 24px;
+    }
+
+    ul {
+      display: none;
+    }
+  }
+
+    @media (max-width: 480px) {
+        header{
+            padding: 0 16px;
+            justify-content: center;
+        }
+
+        #desktop-menu{
+            display: none;
+        }
+
+        #mobile-menu{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .pescoujobs-logo-retractable {
+            display: block;
+        }
     }
 </style>
