aa260bb9c43b59b0dbb70c8ab5031da20a453143
Samuel-Duque <samuellocatel@gmail.com>
2023-10-26 15:23:22 -0300

    Criação do toggle switch

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
     import Header from '$lib/components/Header.svelte';
+    import SlideButton from '$lib/components/SlideButton.svelte';
 </script>
+<SlideButton>
+    <slot/>
+</SlideButton>
 <Header>
-<slot/>
+    <slot/>
 </Header>
