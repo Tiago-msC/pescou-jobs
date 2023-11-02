881523adfd4f1f80df9f94e24b451ae7f049adb3
Samuel-Duque <samuellocatel@gmail.com>
2023-11-02 11:27:19 -0300

    Atualização input 2

--- src/lib/components/input-service-location.svelte
+++ src/lib/components/input-service-location.svelte
@@ -12,17 +12,40 @@
         </div>
     </form>
 </div>
+<div class="caixa-formulario2">
+    <form action="" class="formu">
+        <div class="icon-and-label">
+            <img src={logo} alt="icone" class="icone">
+            <label for="nome-completo">O que você precisa encontrar?</label>
+        </div>
+        <div class="input-container">
+            <input name="nome-completo" placeholder="Digite o serviço desejado (ex: Eletricista, Encanador, Babá)..." type="email" required />
+        </div>
+    </form>
+</div>
 
 <style>
     .caixa-formulario {
         width: 710px;
         height: 87px;
         flex-direction: column;
-        margin: 20px;
+        margin: 20px 20px 0px 20px;
         padding: 16px 75px 16px 24px ;
         align-items: center;
         border: 1.2px solid #CECECE;
         background: #FFF;
+
+    }
+    .caixa-formulario2 {
+        width: 710px;
+        height: 87px;
+        flex-direction: column;
+        margin: 0px 0px 20px 20px;
+        padding: 16px 75px 16px 24px ;
+        align-items: center;
+        border: 1.2px solid #CECECE;
+        background: #FFF;
+
     }
     .input-container {
         display: flex;
